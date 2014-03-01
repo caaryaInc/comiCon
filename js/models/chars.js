14f9c47b7089ef3c8170bdd1e1e0e23707452aff
nikhil <srivatsa.nikhil@gmail.com>
2014-03-01 18:26:36 -0500

    second commit

--- js/models/chars.js
+++ js/models/chars.js
@@ -1,15 +1,31 @@
 App.chars = DS.Model.extend({
   name: DS.attr('string'),
+  category: DS.attr('string'),
   belongsTo: DS.attr('string')
+
 });
 
 App.chars.FIXTURES = [{
 id: 1,
 name: "SpiderMan",
+category: "Hero",
 belongsTo: "marvel"
 },
 {
 id:2,
 name: "Batman",
+category: "Hero",
+belongsTo: "DC"
+},
+{
+id: 3,
+name: "Lizard",
+category: "Villian",
+belongsTo: "marvel"
+},
+{
+id:4,
+name: "Joker",
+category: "Villian",
 belongsTo: "DC"
 }];
--- js/router.js
+++ js/router.js
@@ -1,13 +1,44 @@
 App.Router.map(function () {
-	this.resource('marvel');
-	this.resource('chars');
+  this.resource('marvel', function() {
+	this.resource('marvelheroes',{ path:'/heroes' }, function() {
+		this.resource('marvelhero', { path:'/:char_id' }, function(){
+		this.route('edit');
+		this.route('delete');
+    });
+	this.route('create');
+	});
+	this.resource('marvelvillians', { path:'/villans' }, function() {
+		this.resource('marvelvillian', { path:'/:char_id' }, function(){
+			this.route('edit');
+			this.route('delete');
+		});
+		this.route('create');
+	});
+	});
+ this.resource('dc', function() {
+	this.resource('dcheroes', { path:'/heroes' }, function() {
+		this.resource('dchero', { path:'/:char_id' }, function(){
+		this.route('edit');
+		this.route('delete');
+    });
+	this.route('create');
+	});
+	this.resource('dcvillians',{ path:'/villans' }, function() {
+		this.resource('dcvillian', { path:'/:char_id' }, function(){
+			this.route('edit');
+			this.route('delete');
+		});
+		this.route('create');
+	});
+	});
 });
 
-App.charsRoute = Ember.Route.Extend()({
-	/* model: function(){
-		return this.store.find('chars');
-	} */
-});
+
+App.MarvelheroesRoute = Ember.Route.Extend()({
+	model: function(){
+		return App.chars.Fixtures;
+	} 
+});  
 
 
 
